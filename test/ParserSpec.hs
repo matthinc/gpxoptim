3efcefa75d31f5d28bbee398aaeec9816efe5048
matthias Rupp <matthias.rupp@posteo.de>
2020-07-11 23:17:39 +0200

    Add more tests for parser

--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -4,14 +4,19 @@ import Test.Hspec
 import ParseGPX
 import Types
 
-minimalGPX = "<gpx><trk><name>test</name><trkseg><trkpt lat=\"48.1382\" lng=\"11.6101\">\
-             \<time>2020-06-17T15:36:09.109Z</time></trkpt></trkseg></trk></gpx>"
 
-minimalGPXExpected = GPX { segments = [ Segment [ Point { lat = 48.1382, lng = 11.6101 } ]], trackName = Just "test" }
+minimal = ("<gpx><trk><name>test</name><trkseg><trkpt lat=\"48.1382\" lng=\"11.6101\">\
+            \<time>2020-06-17T15:36:09.109Z</time></trkpt></trkseg></trk></gpx>",
+            GPX { segments = [ Segment [ Point { lat = 48.1382, lng = 11.6101 } ]], trackName = Just "test" })
+
+empty = ("<gpx><trk><trkseg></trkseg></trk></gpx>",
+          GPX { segments = [ Segment []], trackName = Nothing })
 
 spec :: Spec
 spec = do
   describe "parse gpx file" $ do
-    it "parse minimal gpx file example" $
-      (parseGPX minimalGPX) `shouldBe` (Right minimalGPXExpected)
+    it "parse minimal gpx file" $
+      (parseGPX $ fst minimal) `shouldBe` (Right $ snd minimal)
+    it "parse empty gpx file" $
+      (parseGPX $ fst empty) `shouldBe` (Right $ snd empty)
       
