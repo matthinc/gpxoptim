95978af8e9c77f2d8273c1266613ff3599b1409f
matthias Rupp <matthias.rupp@posteo.de>
2020-08-02 21:28:05 +0200

    Tweak track visualization

--- src/Visualization.hs
+++ src/Visualization.hs
@@ -9,19 +9,20 @@ visualizeTrack :: GPX -> Int -> Int -> [String]
 visualizeTrack track width height = map (\n -> row n) $ take height [height, height-1..]
   where grid = scaleToGrid track width height
         row num = concatMap (\n -> (cell n num)) $ take width [1,2..]
-        cell x y = if Set.member (x, y) grid then "." else " "
+        cell x y = if Set.member (x, y) grid then "•" else " "
 
 drawMap :: GPX -> Int -> Int -> String
 drawMap track width height = frameTop ++ concatMap frameLine (visualizeTrack track width height) ++ frameBottom
-  where frameTop = "╔" ++ (replicate width '═') ++ "╗\n"
+  where frameTop = "╠" ++ (replicate width '═') ++ "╣\n"
         frameBottom = "╚" ++ (replicate width '═') ++ "╝"
         frameLine content = "║" ++ content ++ "║\n"
 
 writeData :: GPX -> String
-writeData track = "======== " ++ name ++ " ========\n" ++
-                  "Length:        " ++ (show $ floor $ gpxLength track) ++ "m\n" ++
-                  "GPX segments : " ++ (show $ length $ segments track) ++ "\n"
+writeData track = fillString ("║ Name: " ++ name) 40 ++ "║\n" ++
+                  "║ Length:        " ++ (show $ floor $ gpxLength track) ++ "m\n" ++
+                  "║ GPX segments:  " ++ (show $ length $ segments track)
   where name = case trackName track of
           Nothing -> "(no name)"
           Just n -> n
+        fillString str len = str ++ (replicate (len - length str) ' ')
 
